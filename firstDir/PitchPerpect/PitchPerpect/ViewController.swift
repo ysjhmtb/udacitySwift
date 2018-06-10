35b8175250a3f0c576f0b91094cdc911ad48e068
YSJ <ysjhmtb@gmail.com>
2018-06-10 21:43:54 +0900

    Start a smooth voyage
    being learened iOS development using Swift.
    
    view -> IBAction -> code
    view <- IBOutlet <- code
    
    looking forward to next lecture. Thanks Udacity.

--- firstDir/PitchPerpect/PitchPerpect/ViewController.swift
+++ firstDir/PitchPerpect/PitchPerpect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,14 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+        
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop recording button was pressed")
+    }
 }
 
