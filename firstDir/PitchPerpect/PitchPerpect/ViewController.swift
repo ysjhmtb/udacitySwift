56d24d7afc66f634cc36f02be1da79c6933aeb1b
YSJ <ysjhmtb@gmail.com>
2018-06-16 04:17:24 +0900

    //Always check what function and properties that class inherits from its parent class     //     //What we need to do now is figure out how to turn off or disable     //the stopRecognitionButton. We know that the stopRecordingButton     //is a UIButton. But if we look at the documentation for UIButton,     //it's hard to find anything about enabling or turning things on and off.     //So how do we proceed? Remember that when you look at the documentation for     //a class like UIButton, you should always check what function and properties     //that class inherits from its parent class.     //If you scroll to the botton of this documentation, you'll find the     //Relationships section, I'll give you a hint.     //You'll find what what you're looking for if you dig into the UIControl class     //that the UIButton inherits from.     //So try diving into this documentation and see what you find.     //     //UIButton inherits from UIControl and UIControl has isEnabled property     //that we can set to true or false.

--- firstDir/PitchPerpect/PitchPerpect/ViewController.swift
+++ firstDir/PitchPerpect/PitchPerpect/ViewController.swift
@@ -15,6 +15,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var stopRecordingButton: UIButton!
     
     
+    //Always check what function and properties that class inherits from its parent class
+    //
+    //What we need to do now is figure out how to turn off or disable
+    //the stopRecognitionButton. We know that the stopRecordingButton
+    //is a UIButton. But if we look at the documentation for UIButton,
+    //it's hard to find anything about enabling or turning things on and off.
+    //So how do we proceed? Remember that when you look at the documentation for
+    //a class like UIButton, you should always check what function and properties
+    //that class inherits from its parent class.
+    //If you scroll to the botton of this documentation, you'll find the
+    //Relationships section, I'll give you a hint.
+    //You'll find what what you're looking for if you dig into the UIControl class
+    //that the UIButton inherits from.
+    //So try diving into this documentation and see what you find.
+    //
+    //UIButton inherits from UIControl and UIControl has isEnabled property
+    //that we can set to true or false.
+    
+    
     //viewDidLoad is called when your UIViewController is first loaded into
     //memory, even before it's shown on screen.
     //viewWillAppear however is only called
